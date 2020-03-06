db15dca885ce91c633c4c1d4f7fc7e048f4badd1
Prakash Pokhrel <prakash@zentreasury.com>
2020-03-06 23:26:56 +0200

    Ward crud made

---
+++ Laravel/app/Http/Controllers/Location/WardController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Location;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use App\Cakeapp\Location\Model\Ward;
+use Illuminate\Http\Request;
+
+class WardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $ward = Ward::where('title', 'LIKE', "%$keyword%")
+                ->orWhere('content', 'LIKE', "%$keyword%")
+                ->latest()->paginate($perPage);
+        } else {
+            $ward = Ward::latest()->paginate($perPage);
+        }
+
+        return view('Location.ward.index', compact('ward'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('Location.ward.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        Ward::create($requestData);
+
+        return redirect('ward')->with('flash_message', 'Ward added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $ward = Ward::findOrFail($id);
+
+        return view('locality.ward.show', compact('ward'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $ward = Ward::findOrFail($id);
+
+        return view('locality.ward.edit', compact('ward'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $requestData = $request->all();
+        
+        $ward = Ward::findOrFail($id);
+        $ward->update($requestData);
+
+        return redirect('ward')->with('flash_message', 'Ward updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Ward::destroy($id);
+
+        return redirect('ward')->with('flash_message', 'Ward deleted!');
+    }
+}
