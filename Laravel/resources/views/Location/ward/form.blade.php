f2e9874122b72a66114c3480fad9b020a0cc398f
Prakash Pokhrel <prakash@zentreasury.com>
2020-03-07 08:34:32 +0200

    Ward view file fix

--- Laravel/resources/views/Location/ward/form.blade.php
+++ Laravel/resources/views/Location/ward/form.blade.php
@@ -1,12 +1,10 @@
-<div class="form-group {{ $errors->has('ward_number') ? 'has-error' : ''}}">
+<div class="form-group">
     {!! Form::label('ward_number', 'Ward number', ['class' => 'control-label']) !!}
     {!! Form::text('title', null, ('' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
+<div class="form-group}">
     {!! Form::label('content', 'Content', ['class' => 'control-label']) !!}
     {!! Form::textarea('content', null, ('' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
 </div>
 
 
